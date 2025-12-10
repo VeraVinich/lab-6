dd95b1f247295c80a774c32272d98ef761ba6645
Вера <vera.vinichenki30.04@gmail.com>
2025-12-10 11:50:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ SPECTRUM_WARM = "warm"
 SPECTRUM_COOL = "cool"
 SPECTRUM_NEUTRAL = "neutral"
 
+LAMP_EMISSION_POWER = 60
+LAMP_ENERGY_CONSUMPTION = 60
+LAMP_LIFESPAN = 2000
+
+DAYLIGHT_LAMP_EMISSION_POWER = 30
+DAYLIGHT_LAMP_ENERGY_CONSUMPTION = 10
+DAYLIGHT_LAMP_LIFESPAN = 5000
+
+FLOODLIGHT_EMISSION_POWER = 500
+FLOODLIGHT_ENERGY_CONSUMPTION = 550
+FLOODLIGHT_LIFESPAN = 3000
+FLOODLIGHT_BEAM_ANGLE = 120
+
 
 class Lamp:
 
@@ -25,7 +38,7 @@ class DaylightLamp(Lamp):
         super().__init__(emission_power, energy_consumption, lifespan)
         self.spectrum_type = spectrum_type
 
- 
+
 class Floodlight(Lamp):
 
     def __init__(self, emission_power, energy_consumption, lifespan, beam_angle):
@@ -35,18 +48,27 @@ class Floodlight(Lamp):
 
 # Пример использования
 if __name__ == '__main__':
-    lamp = Lamp(60, 60, 2000)
+    lamp = Lamp(LAMP_EMISSION_POWER, LAMP_ENERGY_CONSUMPTION, LAMP_LIFESPAN)
     print(f"Лампа перегорит через {lamp.days_until_burnout()} дней.")
     print(f"Соотношение мощности к потреблению для лампы: "
           f"{lamp.power_to_consumption_ratio():.2f}\n")
 
-    daylight_lamp = DaylightLamp(30, 10, 5000, SPECTRUM_WARM)
+    daylight_lamp = DaylightLamp(
+        DAYLIGHT_LAMP_EMISSION_POWER,
+        DAYLIGHT_LAMP_ENERGY_CONSUMPTION,
+        DAYLIGHT_LAMP_LIFESPAN,
+        SPECTRUM_WARM)
+    
     print(f"Лампа дневного света перегорит через {daylight_lamp.days_until_burnout()} дней.")
     print(f"Соотношение мощности к потреблению для лампы дневного света: "
           f"{daylight_lamp.power_to_consumption_ratio():.2f}\n")
 
-    floodlight = Floodlight(500, 550, 3000, 120)
+    floodlight = Floodlight(
+        FLOODLIGHT_EMISSION_POWER,
+        FLOODLIGHT_ENERGY_CONSUMPTION,
+        FLOODLIGHT_LIFESPAN,
+        FLOODLIGHT_BEAM_ANGLE)
+    
     print(f"Прожектор перегорит через {floodlight.days_until_burnout()} дней.")
     print(f"Соотношение мощности к потреблению для прожектора: "
-
           f"{floodlight.power_to_consumption_ratio():.2f}")
